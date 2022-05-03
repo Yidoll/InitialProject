869a597a58be30903d2313afc4041b672b5235e7
caiyidong <caiyidong@zhizh.com>
2022-05-03 18:05:23 +0800

    knn

--- algorithmn/knn.py
+++ algorithmn/knn.py
@@ -12,8 +12,8 @@ iris_y = iris.target # 类别部分
 randomarr = np.random.permutation(len(iris_x))
 iris_x_train = iris_x[randomarr[:-10]] # 训练集数据
 iris_y_train = iris_y[randomarr[:-10]] # 训练集标签
-iris_x_test = iris_x[randomarr[-10]] # 测试集数据
-iris_y_test = iris_y[randomarr[-10]] # 测试集标签
+iris_x_test = iris_x[randomarr[-10:]] # 测试集数据
+iris_y_test = iris_y[randomarr[-10:]] # 测试集标签
 
 # 定义一个knn分类器对象
 knn = KNeighborsClassifier()
@@ -21,13 +21,13 @@ knn = KNeighborsClassifier()
 knn.fit(iris_x_train, iris_y_train)
 
 # 调用预测方法，主要接收一个参数：测试数据集
-iris_y_predict = knn.predit(iris_x_test)
+iris_y_predict = knn.predict(iris_x_test)
 
 # 计算各测试样本预期的概率值，这里我们没有用概率值，但是在实际工作总可能会参考概率值来进行最后结果的筛选，而不是直接使用给出的预测标签
 probility = knn.predict_proba(iris_x_test)
 
 # 计算与最后一个测试样本距离最近的5个点，返回的是这些样本的序号组成的数组
-neighborpoint = knn.keighbors([iris_x_test[-1]], 5)
+neighborpoint = knn.kneighbors([iris_x_test[-1]], 5)
 
 # 调用该对象的打分方法，计算出准确率
 score = knn.score(iris_x_test, iris_y_test, sample_weight=None)
